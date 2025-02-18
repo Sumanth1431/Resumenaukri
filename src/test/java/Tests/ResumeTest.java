a399b644f0ba930ae84bd27951c54775adcab829
Sumanth <uniquesumanth143@gmail.com>
2025-02-18 17:33:26 +0530

    code updated

--- src/test/java/Tests/ResumeTest.java
+++ src/test/java/Tests/ResumeTest.java
@@ -13,7 +13,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ResumeBaseTest {
+public class ResumeTest {
 
 	private WebDriver driver;
 	private ConfigLoader config;
